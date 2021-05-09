a03609d196951482124a45c7d8b4d9de3f4e723b
Aaron 亚伦 <rootisalone@hotmail.com>
2021-05-09 21:20:44 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -19,7 +19,7 @@ module.exports = {
   // social usernames (optional)
   twitter: 'dirtywatch',
   github: 'Arstman',
-  linkedin: 'arstman',
+  linkedin: 'puritans',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
