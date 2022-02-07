cf1fc7c3c48766c6a806e7fa6f83d97960830372
彭亚伦 <rootisalone@hotmail.com>
2022-02-07 10:03:26 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: 'In-Rust-Aaron-Trust-102c25fba667474ab5f143a022cf6569',
+  rootNotionPageId: 'In-Rust-Aaron-Trust-65bfe211d3f14f1592edc116e5a19426',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
